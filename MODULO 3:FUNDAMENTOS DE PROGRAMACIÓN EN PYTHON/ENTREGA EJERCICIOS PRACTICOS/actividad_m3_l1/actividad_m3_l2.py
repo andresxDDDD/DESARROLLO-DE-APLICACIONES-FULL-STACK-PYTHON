60d28d08dfc1912f6b10dddee5855684add6f80f
Andres-ux2026 <moraga_2006@hotmail.com>
2026-03-11 14:05:04 +0000

    avance entrega ejercicio leccion2

--- MODULO 3:FUNDAMENTOS DE PROGRAMACIÓN EN PYTHON/ENTREGA EJERCICIOS PRACTICOS/actividad_m3_l1/actividad_m3_l2.py
+++ MODULO 3:FUNDAMENTOS DE PROGRAMACIÓN EN PYTHON/ENTREGA EJERCICIOS PRACTICOS/actividad_m3_l1/actividad_m3_l2.py
@@ -1,6 +1,17 @@
 a=[5,1,4,9,0]
-b= range(3,10) + range(20,23)
+b = list(range(3, 10)) + list(range(20, 23))
 c= [[1,2],[3,4,5],[6,7]]
 d= ['perro','gato','jirafa','elefante']
 e= ['a',a,2*a]
 
+print(a[2])
+print(b[9])
+print(c[1][2])
+
+if e[0] == e[1]:
+    print('true')
+else:
+    print('false')    
+
+print(len(c))   
+print(len(c[0]))  
