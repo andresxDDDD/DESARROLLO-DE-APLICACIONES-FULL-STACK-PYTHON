7657bf2cc0231fb2b5860132f328ae10403f0a3f
andresxDDDD <moraga_2006+1@hotmail.com>
2026-04-12 01:59:33 +0000

    avance leccion 4

--- MODULO 4:PROGRAMACIÓN AVANZADA EN PYTHON (V2)/actividad_m4_l4.py
+++ MODULO 4:PROGRAMACIÓN AVANZADA EN PYTHON (V2)/actividad_m4_l4.py
@@ -1,58 +1,83 @@
 
 
 class Persona:
-    def __init__(self,rut,nombre,apellido):
+   
+    def __init__(self,rut,nombre,apellido,tipo):
         self.rut = rut
         self.nombre = nombre
         self.apellido = apellido
-
-    def ingresar_persona(self):
-
-     pass
-
-    
-             
-
-
-class Alumno(Persona):
-   def __init__(self, rut, nombre, apellido,tipo):
-      super().__init__(rut, nombre, apellido)
-      self.tipo = tipo
-
-   def mostrar(self):
-      return (f"Rut:{self.rut}\n"
+        self.tipo = tipo
+       
+    def mostrar_datos(self):#poliformismo
+         return (f"Rut:{self.rut}\n"
               f"nombre:{self.nombre}\n"
               f"Apellido:{self.apellido}\n"
               f"Tipo:{self.tipo}\n")
         
-   
-class Profesor(Persona):
-   def __init__(self, rut, nombre, apellido,tipo):
-      super().__init__(rut, nombre, apellido)
-      self.tipo = tipo
 
-   def mostrar(self):
-      return (f"Rut:{self.rut}\n"
+    
+class Alumno(Persona): #hereda de la clase persona
+   def __init__(self,persona):
+      super().__init__(persona.rut,persona.nombre,persona.apellido,persona.tipo)
+
+   def mostrar_datos(self):#poliformismo
+         return (f"Rut:{self.rut}\n"
+              f"nombre:{self.nombre}\n"
+              f"Apellido:{self.apellido}\n")
+             
+
+      
+   
+class Profesor(Persona): #hereda de la clase persona
+    def __init__(self,persona,especialidad):
+      super().__init__(persona.rut,persona.nombre,persona.apellido,persona.tipo)
+      self.especialidad = especialidad
+      self.curso_cargo = None
+   
+    def mostrar_datos(self):#poliformismo
+         if self.curso_cargo:
+            nombre_del_curso = self.curso_cargo.nom_curso
+         else:
+            nombre_del_curso = "Sin curso asignado"
+         return (f"Rut:{self.rut}\n"
               f"nombre:{self.nombre}\n"
               f"Apellido:{self.apellido}\n"
-              f"Tipo:{self.tipo}\n")
+              f"Especialidad:{self.especialidad}\n"
+              f"Curso a cargo:{nombre_del_curso}")   
 
 
 
 class Curso:
-   def __init__(self,cod_curso,nom_curso,profesor_jefe,asignatura):
+   def __init__(self,cod_curso,nom_curso):
       self.cod_curso = cod_curso
       self.nom_curso = nom_curso
-      self.profesor_jefe = profesor_jefe
-      self.asignatura = asignatura
+      self.profesor_jefe = None
+      self.asignatura_curso = []
+      self.alumnos_matriculados = []
 
 
-   def mostrar(self):
+
+
+   def mostrar_info(self):
+      nombres_asignaturas = ""
+      for asig in self.asignatura_curso:
+            nombres_asignaturas += f"{asig.nombre}, "
       return (f"Codigo del curso:{self.cod_curso}\n"
               f"Nombre curso:{self.nom_curso}\n"
-              f"Profesor jefe:{self.profesor_jefe}\n"
-              f"Asignaturas:{self.asignatura.nombre}\n")
+              f"Profesor jefe:{self.profesor_jefe.nombre}\n"
+              f"Asignaturas:{nombres_asignaturas}\n"
+              f"Cantidad de alumnos:{len(self.alumnos_matriculados)}")
+   
 
+   def agregar_alumno(self, nuevo_alumno):
+        self.alumnos_matriculados.append(nuevo_alumno)
+
+   def agregar_asignatura(self, nueva_asignatura):
+        self.asignatura_curso.append(nueva_asignatura)  
+
+   def asignar_profesor(self, nuevo_profesor):
+         self.profesor_jefe = nuevo_profesor
+         nuevo_profesor.curso_cargo = self  
 
 class Asignatura: 
    def __init__(self,cod_asignatura,nombre):
@@ -60,11 +85,33 @@ class Asignatura:
       self.nombre = nombre
       
 
+persona1= Persona("16551814-4","Andres","Moraga","Alumno")
+persona2= Persona("11551614-4","Monica","Escobar","Alumno")
+persona3= Persona("15814478-5","roberto","cox","profesor")
+persona4= Persona("8565789-4","Angelica","Gallardo","Auxiliar")
+persona5= Persona("10478956-7","Marta","Loncomilla","profesor")
+alumno1= Alumno(persona1)
+alumno2= Alumno(persona2)
+profesor1= Profesor(persona3,"Lenguaje")
+profesor2= Profesor(persona5,"Lenguaje")
+asignatura1= Asignatura(1,"Lenguaje")
+asignatura2= Asignatura(2,"Matematicas")
+asignatura3= Asignatura(3,"Musica")
+curso1= Curso(1,"1° basico")
+curso1.agregar_alumno(alumno1)
+curso1.agregar_alumno(alumno2)
+curso1.agregar_asignatura(asignatura1)
+curso1.agregar_asignatura(asignatura2)
+curso1.agregar_asignatura(asignatura3)
+curso1.asignar_profesor(profesor2)
 
-asignatura1= Asignatura(1,"Lenjuaje")
-curso1= Curso(1,"1° basico","roberto",asignatura1)
-Alumno1= Alumno("16551814-4","andres","moraga","alumno")
-profesor1= Profesor("15814-478-5","roberto","cox","profesor")
-print(Alumno1.mostrar())
-print(profesor1.mostrar())
-print(curso1.mostrar())
+
+print(alumno1.mostrar_datos())
+print("__________________________")
+print(alumno2.mostrar_datos()) 
+print("__________________________")
+print(profesor2.mostrar_datos())
+print("__________________________")
+print(profesor1.mostrar_datos())
+print("__________________________")
+print(curso1.mostrar_info())
